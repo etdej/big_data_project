d7d9f4ef965aa0eb65bab9a20cc694f81bda5002
Etienne Dejoie <etiennedejoie@gmail.com>
2018-04-13 10:42:31 -0400

    refactor

--- interface.py
+++ interface.py
@@ -21,7 +21,10 @@ base_path='/user/bigdata/nyc_open_data/'
 class Column_selecter:
     def __init__(self, datasets_path):
         self.paths = [os.path.join(base_path, path) for path in datasets_path]
-        df = spark.read.json(self.paths[0], multiLine=True)
+        self.DataFrames = [self.get_dataframe(path) for path in self.paths
+   
+    def get_dataframe(self, path):
+        df = spark.read.json(path, multiLine=True)
         df.registerTempTable("df")
         
         dfCol = df.select(explode(df.meta.view.columns))
@@ -29,25 +32,17 @@ class Column_selecter:
         
         columns = [i.fieldName for i in dfCol.collect()]
 
-        print(columns)
-        #print(dfCol.printSchema())
-        #print(dfCol.show())
-         #print(df.show())
         dfData = df.select(explode(df.data))
-        print(dfData.show())
         dfData = dfData.select(*[dfData.col[i] for i in range(len(columns))]).toDF(*columns)
-        print(dfData.printSchema())
         print(dfData.show())
-        #dfMeta = df.select(df.meta)
-        #print(dfMeta.show())
-        #print(self.data_set0.select('columns'))
-	#print(self.data_set0.get('data'))
-        #data = json.load(open(self.paths[0]))
-        #print(data['columns'])
-        #print(data['data'])
+	return dfData
 
+    def get_columns(self, with=None, without=None):
+        #return the datasets idx and the columns that contain with but do not contain without
+        return
 if __name__ == '__main__':
     cs = Column_selecter(['zwt9-6u9n.json'])
+    print(cs.get_columns(with='BRONX', without='23)'
 
 spark.clearActiveSession()
 spark.clearDefaultSession()
